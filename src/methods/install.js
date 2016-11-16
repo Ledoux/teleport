1f74fe734a902baa4b6305e13d41a4abaf5177f7
Ledoux <erwan.ledoux@snips.net>
2016-11-16 16:07:02 +0100

    fixed install ports

--- src/methods/install.js
+++ src/methods/install.js
@@ -1,6 +1,6 @@
 import childProcess from 'child_process'
 import fs from 'fs'
-import { values } from 'lodash'
+import { toPairs, values } from 'lodash'
 import path from 'path'
 
 import { formatString } from '../utils'
@@ -158,28 +158,40 @@ export function installServer () {
 export function installPorts () {
   this.checkProject()
   this.checkWeb()
-  this.availablePortsByDockerName = {}
-  this.setAllTypesAndServersEnvironment()
-  Object.keys(this.availablePortsByDockerName)
-        .forEach(dockerName => {
-          this.availablePortsByDockerName[dockerName] = this.getAvailablePorts(dockerName)
-        })
-  const { backend } = this
-  Object.keys(backend.serversByName)
-    .forEach((serverName, index) => {
-      const server = backend.serversByName[serverName]
-      values(server.runsByTypeName).forEach(run => {
-        if (this.availablePortsByDockerName[run.dockerName]) {
-          const availablePorts = this.availablePortsByDockerName[run.dockerName]
-          if (availablePorts.length < 1) {
-            this.consoleWarn('Unfortunately, there are not enough available ports for your services... You need to get some as free before.')
-            process.exit()
+  const { project: { config, dir } } = this
+  this.availablePortsBySubDomain = {}
+  values(config.typesByName)
+  .forEach(type => {
+    if (type.subDomain) {
+      this.availablePortsBySubDomain[type.subDomain] = this.getAvailablePorts(type.subDomain)
+    }
+  })
+  if (config.backend && config.backend.serversByName) {
+    Object.keys(config.backend.serversByName)
+      .forEach((serverName, index) => {
+        const server = config.backend.serversByName[serverName]
+        Object.keys(config.typesByName).forEach(typeName => {
+          let run = server.runsByTypeName[typeName]
+          if (typeof run === 'undefined') {
+            run = server.runsByTypeName[typeName] = {}
           }
-          run.port = availablePorts[0].toString()
-          this.availablePortsByDockerName[run.dockerName] = availablePorts.slice(1)
-        }
+          const subDomain = run.subDomain || config.typesByName[typeName].subDomain
+          if (typeof subDomain === 'undefined') {
+            return
+          }
+          if (this.availablePortsBySubDomain[subDomain]) {
+            const availablePorts = this.availablePortsBySubDomain[subDomain]
+            if (availablePorts.length < 1) {
+              this.consoleWarn('Unfortunately, there are not enough available ports for your services... You need to get some as free before.')
+              process.exit()
+            }
+            run.port = availablePorts[0].toString()
+            this.availablePortsBySubDomain[subDomain] = availablePorts.slice(1)
+          }
+        })
       })
-    })
+  }
+  this.writeConfig(dir, config)
 }
 
 export function installPlaceholderFiles () {
@@ -268,6 +280,7 @@ export function installPlaceholderFile () {
   const extraConfig = Object.assign(
     {
       'DOCKER_HOST': run.host,
+      'PORT': run.port,
       'SITE_NAME': backend.siteName,
       'TYPE': type.name,
       'URL': run.url,
