f03b49c8c65ca34144df665da73e60d8de7f688d
Ledoux <erwan.ledoux@snips.net>
2016-11-18 18:35:29 +0100

    better gitignore

--- src/methods/deploy.js
+++ src/methods/deploy.js
@@ -7,43 +7,11 @@ export function deploy () {
   if (program.type === 'localhost') {
     program.type = 'staging'
   }
-  /*
-  if (program.server && program.type) {
-    this.deployServer()
-  } else {
-    this.deployServers()
-  }
-  */
-  childProcess.execSync(
-    `tpt -e --script deploy --type ${program.type} --servers all --ttab true`
-  )
-}
-
-export function deployServerInNewTab () {
-  const { app, program, server, type } = this
-  const command = `${app.ttabDir} tpt -d --server ${server.name} --type ${type.name} --cache ${program.cache}`
-  console.log(childProcess.execSync(command).toString('utf-8'))
-}
-
-export function deployServers () {
-  const { program } = this
-  program.method = 'deployServer'
+  let command = `tpt -e --script deploy --type ${program.type} --servers all`
   if (program.user === 'me') {
-    program.method = `${program.method}InNewTab`
+    command = `${command} --ttab true`
   }
-  program.methods = null
-  this.setTypeEnvironment()
-  this.mapInServers()
-}
-
-export function deployServer () {
-  const { program } = this
-  this.buildDocker()
-  this.pushDocker()
-  if (program.image !== 'base') {
-    this.registerDocker()
-    this.restartDocker()
-  }
+  childProcess.execSync(command)
 }
 
 export function getUsedPorts () {
@@ -60,107 +28,6 @@ export function checkPort () {
   if (typeof this.usedPorts === 'undefined') {
     this.usedPorts = this.getUsedPorts()
   }
-}
-
-/*
-export function getBuildDockerCommand (config) {
-  this.checkProject()
-  const { backend, project, program, run, server, type } = this
-  let fileName = 'Dockerfile'
-  if (program.image) {
-    fileName = `${program.image}_${fileName}`
-  }
-  if (type) {
-    fileName = `${type.name}_${fileName}`
-  }
-  const imageName = program.image
-  ? server[`${program.image}Image`]
-  : run.image
-  const cache = program.cache === 'true'
-    ? ''
-    : '--no-cache'
-  return [
-    `cd ${server.dir}`,
-    `docker ${backend.buildPushSocket} build -f ${fileName} -t ${imageName} ${cache} .`,
-    `cd ${project.dir}`
-  ].join(' && ')
-}
-*/
-
-export function buildDocker () {
-  this.checkWeb()
-  this.checkPort()
-  const { server, type } = this
-  /*
-  const command = this.getBuildDockerCommand()
-  this.consoleInfo(`Ok we build your docker image of ${type.name} ${program.image ? program.image : ''} ${server.name}...
-    can take a little of time...`)
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
-  */
-  const command = `cd ${server.dir} && sh scripts/${type.name}_build.sh`
-  this.consoleInfo('Let\'s install the project')
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
-}
-
-/*
-export function getPushDockerCommand (config) {
-  this.checkProject()
-  const { backend, project, program, run, server } = this
-  let imageName
-  if (program.image) {
-    imageName = server[`${program.image}Image`]
-  } else if (run) {
-    imageName = run.image
-  }
-  const command = [
-    `cd ${server.dir}`,
-    `docker ${backend.buildPushSocket} push ${imageName}`,
-    `cd ${project.dir}`
-  ].join(' && ')
-  return command
-}
-*/
-
-export function pushDocker () {
-  this.checkWeb()
-  const { server, type } = this
-  // this.checkPort()
-  /*
-  const command = this.getPushDockerCommand()
-  this.consoleInfo(`Ok we push your docker image...
-    can take a little of time...`)
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
-  */
-  const command = `cd ${server.dir} && sh scripts/${type.name}_push.sh`
-  this.consoleInfo('Let\'s install the project')
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
-}
-
-export function getRegisterDockerCommand (config) {
-  this.checkProject()
-  const { app, project, run, server, type } = this
-  if (!run) return
-  const serviceYamlPath = `${server.configDir}/${type.name}_service.yaml`
-  const controllerYamlPath = `${server.configDir}/${type.name}_controller.yaml`
-  return [
-    `python ${app.pythonDir} register ${serviceYamlPath}`,
-    `python ${app.pythonDir} register ${controllerYamlPath}`,
-    `cd ${project.dir}`
-  ].join(' && ')
-}
-
-export function registerDocker () {
-  this.checkWeb()
-  this.checkPort()
-  const command = this.getRegisterDockerCommand()
-  this.consoleInfo(`Ok we register your docker image...
-    can take a little of time...`)
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
 }
 
 export function getRestartDockerCommand (config) {
