5cc84db8ff0f6dfdad3ae44664786103f0dd3869
Ledoux <erwan.ledoux@snips.net>
2017-01-12 16:02:04 +0100

    made regex to grab placeholder files

---
+++ src/methods/push.js
@@ -0,0 +1,19 @@
+import childProcess from 'child_process'
+
+export function push () {
+  const { program } = this
+  // type is localhost by default, but here we want to deploy
+  // so we set actually the default to staging here
+  if (program.type === 'localhost') {
+    program.type = 'staging'
+  }
+  let commands = []
+  commands.push(`tpt -e --script push --type ${program.type} --servers all`)
+  let command = commands.join(' && ')
+  if (program.user === 'me') {
+    command = `${command} --ttab true`
+  }
+  this.consoleInfo('Let\'s push')
+  this.consoleLog(command)
+  console.log(childProcess.execSync(command).toString('utf-8'))
+}
