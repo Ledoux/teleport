e52dc25b26a124ed0c85656a5d4da8dc483cd923
Ledoux <erwan.ledoux@snips.net>
2017-01-19 16:29:58 +0100

    replaced ttab by concurrent

--- src/methods/push.js
+++ src/methods/push.js
@@ -2,6 +2,8 @@ import childProcess from 'child_process'
 
 export function push () {
   const { program } = this
+  // reset
+  this.execResetConcurrently('push')
   // type is localhost by default, but here we want to deploy
   // so we set actually the default to staging here
   if (program.type === 'localhost') {
@@ -10,10 +12,7 @@ export function push () {
   let commands = []
   commands.push(`tpt -e --script push --type ${program.type} --servers all`)
   let command = commands.join(' && ')
-  if (program.user === 'me') {
-    command = `${command} --ttab true`
-  }
-  this.consoleInfo('Let\'s push')
+  // exec
   this.consoleLog(command)
   console.log(childProcess.execSync(command).toString('utf-8'))
 }
--- src/methods/uninstall.js
+++ src/methods/uninstall.js
@@ -2,21 +2,6 @@ import childProcess from 'child_process'
 
 export function uninstall () {
   this.uninstallPythonDependencies()
-  const { program } = this
-  // type is localhost by default, but here we want to deploy
-  // so we set actually the default to staging here
-  if (program.type === 'localhost') {
-    program.type = 'staging'
-  }
-  let commands = []
-  commands.push(`tpt -e --script uninstall --type ${program.type} --servers all`)
-  let command = commands.join(' && ')
-  if (program.user === 'me') {
-    command = `${command} --ttab true`
-  }
-  this.consoleInfo('Let\'s push')
-  this.consoleLog(command)
-  console.log(childProcess.execSync(command).toString('utf-8'))
 }
 
 export function uninstallPythonDependencies () {
