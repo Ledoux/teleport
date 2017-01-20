ebd8a044312d97db7fb1c59e884601ab4b66ead2
Ledoux <erwan.ledoux@snips.net>
2017-01-20 15:48:00 +0100

    fix deploy step by fixing at the run stage

--- src/methods/run.js
+++ src/methods/run.js
@@ -2,6 +2,8 @@ import childProcess from 'child_process'
 
 export function run () {
   const { program } = this
+  // reset
+  this.execResetConcurrently('push')
   // type is localhost by default, but here we want to deploy
   // so we set actually the default to staging here
   if (program.type === 'localhost') {
@@ -10,10 +12,7 @@ export function run () {
   let commands = []
   commands.push(`tpt -e --script run --type ${program.type} --servers all`)
   let command = commands.join(' && ')
-  if (program.user === 'me' && program.ttab === 'true') {
-    command = `${command} --ttab true`
-  }
-  this.consoleInfo('Let\'s push')
+  // exec
   this.consoleLog(command)
   childProcess.execSync(command, { stdio: [0, 1, 2] })
 }
