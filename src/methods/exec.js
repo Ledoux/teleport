ad294f1adbdefe2ae78431d42a8495f816da4326
Ledoux <erwan.ledoux@snips.net>
2017-01-23 16:24:07 +0100

    fix the kubernetes set

--- src/methods/exec.js
+++ src/methods/exec.js
@@ -21,7 +21,7 @@ export function exec () {
     if (platformScriptNames.includes(script) && platform &&
       // NOTE : this is where we need to do a little hacky workaround
       // our default platform value is heroku, but our scripts in our templates
-      // are by default kubernetes if they don't have a prefix platform 
+      // are by default kubernetes if they don't have a prefix platform
       platform !== 'kubernetes') {
       typedScript = `${platform}_${script}`
     }
