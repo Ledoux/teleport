472637af8eef014e4d1525d955fad23c2e5eb10c
Ledoux <erwan.ledoux@snips.net>
2017-01-25 18:04:43 +0100

    made teleport invent a uniq id name for project

--- src/utils/index.js
+++ src/utils/index.js
@@ -137,3 +137,12 @@ export function getCartesianProduct (...args) {
     }), true)
   }, [ [] ])
 }
+
+export function id () {
+  function s4 () {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4()
+}
