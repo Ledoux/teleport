117e9a5f450073a5b8e767dadcedc3257b42ec75
erwanledoux <lawanledoux@gmail.com>
2017-01-30 17:15:22 +0100

    added a methods jest test

---
+++ __tests__/methods.js
@@ -0,0 +1,28 @@
+const childProcess = require('child_process')
+const Teleport = require('../lib').default
+
+const CREATION_COMMAND = 'tpt -c --name my-app --templates teleport-express-webrouter,teleport-flask-websocket'
+
+/*
+'tpt -e --script ping --servers express-webrouter,flask-websocket'
+'tpt -e --script ping --servers all'
+'tpt -e --method getConfig'
+'tpt -g --kwarg project.config'
+'tpt map --method install --collections project.config.backend.serversByName'
+'tpt map --method get --kwarg run --collections project.config.typesByName,project.config.backend.serversByName'
+*/
+
+/*
+test('execute utility method', () => {
+  const testTeleport = new Teleport({})
+  console.log(childProcess.execSync(CREATION_COMMAND).toString('utf-8'))
+})
+*/
+
+test('execute utility method', () => {
+  const testTeleport = new Teleport({
+    create: true,
+    name: 'my-app',
+    templates: 'teleport-express-webrouter,teleport-flask-websocket'
+  })
+})
