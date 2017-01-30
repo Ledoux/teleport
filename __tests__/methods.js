5dc41635547a79ed5093a20e681d42f6a41a753b
erwanledoux <lawanledoux@gmail.com>
2017-01-30 18:43:58 +0100

    added test for exec get map

--- __tests__/methods.js
+++ __tests__/methods.js
@@ -1,25 +1,14 @@
 const childProcess = require('child_process')
+const path = require('path')
+
 const Teleport = require('../lib').default
 
-const CREATION_COMMAND = 'tpt -c --name my-app --templates teleport-express-webrouter,teleport-flask-websocket'
+const TEST_APP_NAME = 'my-test-app'
+const TEST_APP_DIR = path.join(process.cwd(), TEST_APP_NAME)
 
 /*
-'tpt -e --script ping --servers express-webrouter,flask-websocket'
-'tpt -e --script ping --servers all'
-'tpt -e --method getConfig'
-'tpt -g --kwarg project.config'
-'tpt map --method install --collections project.config.backend.serversByName'
-'tpt map --method get --kwarg run --collections project.config.typesByName,project.config.backend.serversByName'
-*/
-
-/*
-test('execute utility method', () => {
-  const testTeleport = new Teleport({})
-  console.log(childProcess.execSync(CREATION_COMMAND).toString('utf-8'))
-})
-*/
-
-test('execute utility method', () => {
+// equivalent as 'tpt -c --name my-test-app --templates teleport-express-webrouter,teleport-flask-websocket'
+test('create task', () => {
   childProcess.execSync('rm -rf my-test-app')
   const testTeleport = new Teleport({
     create: true,
@@ -27,3 +16,44 @@ test('execute utility method', () => {
     templates: 'teleport-express-webrouter,teleport-flask-websocket'
   }).launch()
 })
+*/
+
+// equivalent as 'tpt -e --method getConfig'
+test('execute utility method', () => {
+  const testTeleport = new Teleport({
+    dir: TEST_APP_DIR,
+    exec: true,
+    method: 'getConfig'
+  }).launch()
+})
+
+/*
+// equivalent as 'tpt -g --kwarg project.config'
+test('get utility method', () => {
+  const testTeleport = new Teleport({
+    dir: TEST_APP_DIR,
+    get: true,
+    kwarg: 'project.config'
+  }).launch()
+})
+
+// equvalent as 'tpt map --method install --collections project.config.backend.serversByName'
+test('map utility method for installing', () => {
+  const testTeleport = new Teleport({
+    dir: TEST_APP_DIR,
+    collections: 'project.config.backend.serversByName'
+    map: true
+  }).launch()
+})
+
+// equvalent as 'tpt map --method get --kwarg run --collections project.config.typesByName,project.config.backend.serversByName'
+test('map utility method for getting the run infos', () => {
+  const testTeleport = new Teleport({
+    dir: TEST_APP_DIR,
+    collections: 'project.config.typesByName,project.config.backend.serversByName'
+    map: true,
+    method: 'get',
+    kwarg: 'run'
+  }).launch()
+})
+*/
