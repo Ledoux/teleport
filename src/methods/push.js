f2cb825e019d386104eeb6c384138c1ceb1463fa
erwanledoux <lawanledoux@gmail.com>
2017-01-30 12:03:43 +0100

    added comments on push

--- src/methods/push.js
+++ src/methods/push.js
@@ -1,3 +1,10 @@
+// PUSH SUB TASK
+// push is called at the deploy task time, but you can also call it in an already
+// created project. It is here about to push the heroku git repositories/docker images
+// of each server that will help then to deploy the whole project.
+// - push goes to each server and execute their scripts/<type>_<platform>_push.sh
+// script.
+
 import childProcess from 'child_process'
 
 export function push () {
@@ -7,7 +14,6 @@ export function push () {
   if (program.type === 'localhost') {
     program.type = 'staging'
   }
-
   const command = `tpt -e --script push --type ${program.type} --platform ${program.platform} --servers all`
   // exec
   this.consoleLog(command)
